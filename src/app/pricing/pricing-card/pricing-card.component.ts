84323b1d4fd2d62b6ac981b28ad39d9818784f9d
lbandin <lucasblandin@hotmail.fr>
2025-02-09 16:36:10 +0100

    Pricing section

---
+++ src/app/pricing/pricing-card/pricing-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { StandardButtonComponent } from '../../common/standard-button/standard-button.component';
+@Component({
+  selector: 'app-pricing-card',
+  imports: [StandardButtonComponent],
+  templateUrl: './pricing-card.component.html',
+  styleUrl: './pricing-card.component.css'
+})
+export class PricingCardComponent {
+
+}
