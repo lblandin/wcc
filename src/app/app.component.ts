bf67ad20151e81fe33168ae699cf6e9b1603fd21
lbandin <lucasblandin@hotmail.fr>
2025-02-12 21:35:29 +0100

    footer branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { FaqComponent } from './faq/faq.component';
 import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, PricingComponent, FaqComponent, AboutComponent],
+  imports: [HeaderComponent, PricingComponent, FaqComponent, AboutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
